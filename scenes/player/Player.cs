b8ba1bfd134be9a0e3ce537f23b038b24891f6b6
ziurj <ruiz07014@gmail.com>
2025-11-12 20:34:07 -0500

    feat: Integrate the new trigger slider

--- scenes/player/Player.cs
+++ scenes/player/Player.cs
@@ -1,22 +1,31 @@
 using Godot;
-using System;
+using System.Threading.Tasks;
 
 public partial class Player : Actionable
 {
     [Export]
-    private Control _playerInterface;
+    private Control _playerInterface, _waveOptionsContainer, _targetSliderContainer;
+
+    [Export]
+    private ActionTargetSlider _targetSlider;
 
     public override void _Ready()
     {
         base._Ready();
+        _targetSlider.SliderStopped += GenerateWaveResource;
     }
 
-    public void EnableInterface(bool enable = true)
+    public void EnableWaveOptionsInterface(bool enable = true)
     {
-        _playerInterface.Visible = enable;
+        _waveOptionsContainer.Visible = enable;
     }
 
-    public void ButtonPressed(string waveName)
+    public void EnableSliderInterface(bool enable = true)
+    {
+        _targetSliderContainer.Visible = enable;
+    }
+
+    public async void ButtonPressed(string waveName)
     {
         // GD.Print("PRESSING PLAYER BUTTON");
         string selectedWave = "";
@@ -40,12 +49,40 @@ public partial class Player : Actionable
                 break;
         }
 
+        EnableWaveOptionsInterface(false);
+
+        EnableSliderInterface();
+        _targetSlider.Enable();
+
+        // Wait for the slider process to finish (this pauses until SliderStopped is emitted)
+        await UpdateAndEmitWaveResourceAsync(selectedWave);
+
+        EnableSliderInterface(false);
+        _targetSlider.Reset();
+
+    }
+
+    private async Task UpdateAndEmitWaveResourceAsync(string selectedWave)
+    {
+        GD.Print("ASYNC REACHEd");
+
+        // WaveRes is inherited.
         if (WaveRes is Wave waveResource)
         {
             waveResource.WaveType = selectedWave;
+            await ToSignal(_targetSlider, nameof(_targetSlider.SliderStopped)); // Wait before alerting the game manager we have finished.
             EmitSignal(nameof(TurnSignal), [ waveResource ]);
+            GD.PrintS($"WAVE RESOURCE: {waveResource.WaveType} // {waveResource.TargetSliderFinalValue} ");
         }
+    }
 
-        EnableInterface(false);
+    private void GenerateWaveResource(int finalSliderValue)
+    {
+        Wave newWaveRes = new()
+        {
+            TargetSliderFinalValue = finalSliderValue
+        };
+
+        WaveRes = newWaveRes;
     }
 }
