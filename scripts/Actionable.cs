6128e11d21c72308c0189d398fea64f309c15307
ziurj <ruiz07014@gmail.com>
2025-11-28 17:57:33 -0500

    feat: Add reference to health bar component

--- scripts/Actionable.cs
+++ scripts/Actionable.cs
@@ -7,4 +7,11 @@ public partial class Actionable : CharacterBody3D
 
 	[Signal]
     public delegate void TurnSignalEventHandler();
+
+    public HealthBar HealthBarComponent;
+
+    public override void _Ready()
+    {
+        HealthBarComponent = GetNode<HealthBar>("HealthBar");
+    }
 }
